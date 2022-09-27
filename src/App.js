e629f58816b1827d91767a2e38ea531e78a623cc
“choanss” <linnea.anne.andersson@gmail.com>
2022-09-27 22:26:53 +0200

    kommenterat ut kontakt

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import Herosec from './components/Herosec';
 import About from './components/About';
 import Contact from './components/Contact'
-import Recentioner from './components/Recentioner';
+// import Recentioner from './components/Recentioner';
 // import Footer from './components/Footer';
 
 function App() {
@@ -11,7 +11,7 @@ function App() {
       <Herosec />
       <About />
       <Contact />
-      <Recentioner />
+      {/* <Recentioner /> */}
  
     </>
   );
